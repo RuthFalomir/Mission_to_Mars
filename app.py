556f8aeaea4ce54012dbd87f870d61025a6cd150
RuthFalomir <88563922+RuthFalomir@users.noreply.github.com>
2021-11-28 12:49:57 -0600

    Adding challenge for Module 10

---
+++ app.py
@@ -0,0 +1,28 @@
+from bs4 import MarkupResemblesLocatorWarning
+from flask import Flask, render_template, redirect, url_for
+from flask_pymongo import PyMongo
+import scraping
+
+app=Flask(__name__)
+
+# Use flask_pymongo to set up mongo connection
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+mongo = PyMongo(app)
+
+@app.route("/")
+
+def index():
+   mars = mongo.db.mars.find_one()
+   return render_template("index.html", mars=mars)
+
+@app.route("/scrape")
+
+def scrape():
+   mars = mongo.db.mars
+   mars_data = scraping.scrape_all()
+   # reviewed at office hours to fine updated way of writing this piece of code.
+   mars.update_many({},{"$set":mars_data} , upsert=True)
+   return redirect('/', code=302), mars_data
+
+if __name__ == "__main__":
+   app.run()
